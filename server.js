0a9e9f9bbb470c5563a6eae934e195daf0661176
Madhav Mittal <heromadhav99@gmail.com>
2020-08-01 22:27:56 +0800

    Connected MongoDB to Mongoose

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('API Running'));
+//Connecting the mongoDB Database to Mongoose
+connectDB();
 
+app.get('/', (req, res) => res.send('API Running')); //if localhost port 5000 server recieves anything, send a message
+//app.get is a route, / points to the root of the domain
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
